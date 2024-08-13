34d47ec1f2426479c27de99655dcd1cabed7b220
Raito-95 <info.raito.site@gmail.com>
2024-08-13 17:48:51 +0800

    Fixed verify code format

--- thsr_ticket/extra/image_process.py
+++ thsr_ticket/extra/image_process.py
@@ -1,7 +1,14 @@
 import ddddocr
+from PIL import Image
 
 ocr = ddddocr.DdddOcr(show_ad=False)
 
-
-def verify_code(image):
-    return ocr.classification(image)
+def verify_code(image: Image.Image) -> str:
+    result = ocr.classification(image)
+    
+    if isinstance(result, str):
+        return result
+    elif isinstance(result, dict):
+        return ''.join(result.get('charsets', []))
+    else:
+        return ""
